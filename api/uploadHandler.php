8165a0695ad26b38ec1521719100a7cdb9a6c7e7
rrahmouni <rrahmouni@90965971-89fd-4c9b-8651-a71ee073ffc4>
2016-03-01 15:44:21 +0000

    test

--- api/uploadHandler.php
+++ api/uploadHandler.php
@@ -3,14 +3,84 @@
  * Upload Hanlder class
  **/
 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
+header("access-control-allow-origin: *");
 
-//TODO exit script from here when no correct posts
+require_once 'autoLoader.php';
 
 
 class uploadHandler {
+
+    /**
+     * Send raw HTTP response
+     * @param int $status HTTP status code
+     * @param string $body The body of the HTTP response
+     * @param string $contentType Header content-type
+     * @return HTTP response
+     */
+    private function sendResponse($status = 200, $body = '', $contentType = 'application/json')
+    {
+        // Set the status
+        $statusHeader = 'HTTP/1.1 ' . $status . ' ' . $this->getStatusCodeMessage($status);
+        header($statusHeader);
+        // Set the content type
+        header('Content-type: ' . $contentType);
+
+        echo $body;
+    }
+
+    /**
+     * Return the http status message based on integer status code
+     * @param int $status HTTP status code
+     * @return string status message
+     */
+
+    private function getStatusCodeMessage($status)
+    {
+        $codes = array(
+            100 => 'Continue',
+            101 => 'Switching Protocols',
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-Authoritative Information',
+            204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            306 => '',
+            307 => 'Temporary Redirect',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            402 => 'Payment Required',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Timeout',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
+            412 => 'Precondition Failed',
+            413 => 'Request Entity Too Large',
+            414 => 'Request-URI Too Long',
+            415 => 'Unsupported Media Type',
+            416 => 'Requested Range Not Satisfiable',
+            417 => 'Expectation Failed',
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Timeout',
+            505 => 'HTTP Version Not Supported'
+        );
+        return (isset($codes[$status])) ? $codes[$status] : '';
+    }
 
     /**
      * Upload directory
@@ -36,52 +106,46 @@ class uploadHandler {
     /**
      * Constructor function.
      */
-    function __construct() {
+    function __construct($fileType) {
+        //set upload directory
         $this->upload_dir = "../uploads/";
 
         //Set folder type from post
-        $this->foldertype = "SDFiles";
-
-        switch($this->foldertype) {
-            case "":break;
-            case "":break;
-            case "":break;
-            case "":break;
-            default:break;
-
-        }
+        //$this->foldertype = "SDFiles";
 
         //set supported mimes array
-        $this->mimes = array('application/vnd.ms-excel','text/plain','text/csv','text/tsv');
+        switch($fileType) {
+            case "image":$this->mimes = array('image/jpeg');
+                break;
+            case "excel":$this->mimes = array('application/vnd.ms-excel','text/plain','text/csv','text/tsv');
+                break;
+            default:$this->mimes = array();break;
+
+        }
     }
 
     /**
      * Upload file
      * @return JSON
      */
+
     function uploadFile() {
 
         if (isset($_FILES["myfile"])) {
 
             if ($_FILES["myfile"]["error"] > 0) {
-                echo json_encode(array('status'=>'error','msg_error'=>$_FILES['file']['error']));
+                $this->sendResponse(200,json_encode(array('status'=>'error','msg'=>$_FILES['file']['error'])));
             } else {
                 if(in_array($_FILES['myfile']['type'],$this->mimes)){
 
                     if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $this->upload_dir . $_FILES["myfile"]["name"])) {
 
-                        $content = file($this->upload_dir . $_FILES["myfile"]["name"]);
-                        $csv = array_map('str_getcsv',$content,array_fill(0, count($content), $this->delemiter) );
-
-                        if($this->createTableFromArray($csv,basename($this->upload_dir . $_FILES["myfile"]["name"], ".csv"))) {
-
-                            echo json_encode(array('status'=>'success','array'=>$csv,'tablename'=>basename($this->upload_dir . $_FILES["myfile"]["name"], ".csv")));
-                        }
-
+                        //$content = file($this->upload_dir . $_FILES["myfile"]["name"]);
+                        $this->sendResponse(200,json_encode(array('status'=>'success','msg'=>'file transfered')));
                     }
 
                 } else {
-                    echo json_encode(array('status'=>'error','msg_error'=>'csv invalid'));
+                    $this->sendResponse(200,json_encode(array('status'=>'error','msg'=>'invalid file type')));
                 }
 
             }
@@ -90,5 +154,7 @@ class uploadHandler {
     }
 }// END class
 
-$api = new uploadHandler();
-$api->uploadFile();
+if(isset($_POST['filetype'])) {
+    $api = new uploadHandler($_POST['filetype']);
+    $api->uploadFile($_POST['filetype']);
+} else $this->sendResponse(200,json_encode(array('status'=>'error','msg'=>'file type not specified')));
