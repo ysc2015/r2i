aff027864d52e20f78bc10a9fa0ce3d192d97887
rrahmouni <rrahmouni@90965971-89fd-4c9b-8651-a71ee073ffc4>
2016-03-07 10:27:07 +0000

    test

--- assets/js/pages/r2i.projects_mod.js.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-type: application/javascript");
-?>
-/*
- *  Document   : r2i.projects_mod.js.php
- *  Author     : RR
- *  Description: Custom JS code used in Admin Page Projects MOD
- */
