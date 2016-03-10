cf83da8d8f46bf04f22c39d087679e3fe1e06939
rrahmouni <rrahmouni@90965971-89fd-4c9b-8651-a71ee073ffc4>
2016-03-10 17:48:01 +0000

    Failed commit: Default

--- assets/js/pages/r2i.utilisateurs_add.js.php
+++ assets/js/pages/r2i.utilisateurs_add.js.php
@@ -8,7 +8,7 @@ header("Content-type: application/javascript");
  */
 
 var ProjectFormValidation = function() {
-    var API_URL = 'public/api/r2iApi.php';
+    var API_URL = 'public/api/r2iApiUser.php';
     var uploadObj = null;
     var selectedFilesCounter = 0;
     //get $_GET array
@@ -30,25 +30,7 @@ var ProjectFormValidation = function() {
         }
         return $_GET;
     };
-    //send mail
-    var sendMail = function() {
-        $.ajax({
-            url: 'public/api/mailApi.php',
-            type: 'POST',
-            dataType: 'json',
-            data: {method : 'send_mail'},
-            success: function (response) {
-                console.log('send_mail:success');
-                console.log(response);
-                alert('Projet enregistré !');
-                window.location.replace('?page=projects');
-            },
-            error: function (e) {
-                console.log('send_mail:error');
-                console.log(e.responseText);
-            }
-        });
-    }
+
     //call ajax -> api method
     var executeMethod = function($methodname, $params) {
         $.ajax({
@@ -59,18 +41,7 @@ var ProjectFormValidation = function() {
             success: function (response) {
                 console.log($methodname+':success');
                 console.log(response);
-                if($methodname == 'insert_project') uploadObj.startUpload();
-                if($methodname == 'insert_file') selectedFilesCounter--;
-                if(selectedFilesCounter == 0 && $methodname == 'insert_file') {
-                    console.log('did finish adding files TO DB');
-                    //TODO temp code before form valildation enable
-                    //send email here
-                    console.log('send mail here !');
-                    sendMail();
-                    //window.location.replace('?page=projects');
-                    //location.reload();
 
-                }
             },
             error: function (e) {
                 console.log($methodname+':error');
@@ -78,45 +49,13 @@ var ProjectFormValidation = function() {
             }
         });
     };
-    //Init JQuery Upload plugin
-    var initUpload = function() {
-        uploadObj = $("#fileuploader").uploadFile({
-            url:"public/api/upload.php",
-            fileName:"myfile",
-            returnType:"json",
-            autoSubmit:false,
-            uploadStr:"Ajouter fichier(s)",
-            cancelStr:"Annuler",
-            dragDropStr:"<span><b>Glissez vos fichiers ici Fichiers</b></span>",
-            afterUploadAll:function(obj)
-            {
-                console.log('afterUploadAll');
-                var $_GET = get_$GET_Array();
-                $.each(obj.existingFileNames, function( index, value ) {
-                    console.log( index + ": " + value );
-                    var $row ={project_id:$_GET['idp'],folder_id:1,filename:value};
-                    executeMethod('insert_file',JSON.stringify($row));
-                });
 
-
-            },
-//            formData: {"idp":"Ravi","idf":31},
-            onSuccess:function(files,data,xhr,pd)
-            {
-                /*var $_GET = get_$GET_Array();
-                 //call ajax
-                 var $row ={project_id:$_GET['idp'],folder_id:1,filename:data.data[0],file_ext:'exx'};
-                 executeMethod('insert_file',JSON.stringify($row));*/
-
-            }
-        });
-    };
     // Add a project
     var addProject = function() {
         var $form = jQuery('.js-validation-bootstrap');
         var $row = {}; // default row value;
         // When the add project form is submitted
-        jQuery('.add-project').on('click', function(){
+        jQuery('.add_utilisateur').on('click', function(){
             console.log('Enregistrement projet');
             console.log(uploadObj);
             if($form.valid()) {
@@ -127,7 +66,7 @@ var ProjectFormValidation = function() {
                     });
                     console.log(JSON.stringify($row));
                     //call ajax
-                    executeMethod('insert_project',JSON.stringify($row));
+                    executeMethod('insert_user',JSON.stringify($row));
                 } else {
                     alert('Ajouter des fichiers au projet !');
                 }
