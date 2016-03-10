cae68ed9bf0a5bfc56b49137108b0677f220d1ce
rrahmouni <rrahmouni@90965971-89fd-4c9b-8651-a71ee073ffc4>
2016-03-10 17:46:59 +0000

    Failed commit: Default

--- views/utilisateurs/utilisateurs_header_inc.php
+++ views/utilisateurs/utilisateurs_header_inc.php
@@ -1,3 +1,13 @@
 <!-- Page JS Plugins CSS -->
-<link rel="stylesheet" href="<?php echo $one->assets_folder; ?>/js/plugins/slick/slick.min.css">
-<link rel="stylesheet" href="<?php echo $one->assets_folder; ?>/js/plugins/slick/slick-theme.min.css">
+<?php if($action == "list"): ?>
+    <link rel="stylesheet" href="<?php echo $one->assets_folder; ?>/js/plugins/datatables/jquery.dataTables.min.css">
+<?php endif; ?>
+<?php if($action == "add"): ?>
+    <link rel="stylesheet" href="<?php echo $one->assets_folder; ?>/js/plugins/bootstrap-datepicker/bootstrap-datepicker3.min.css">
+    <link rel="stylesheet" href="<?php echo $one->assets_folder; ?>/js/plugins/jquery-upload-file/uploadfile.css">
+<?php endif; ?>
+<?php if($action == "sdfiles"): ?>
+    <link rel="stylesheet" href="<?php echo $one->assets_folder; ?>/js/plugins/jquery-upload-file/uploadfile.css">
+<?php endif; ?>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
