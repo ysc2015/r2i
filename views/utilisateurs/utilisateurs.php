1372f984f76f6911019a3e72b307aa973a9c7752
rrahmouni <rrahmouni@90965971-89fd-4c9b-8651-a71ee073ffc4>
2016-03-14 10:14:48 +0000

    Failed commit: Default

--- views/utilisateurs/utilisateurs.php
+++ views/utilisateurs/utilisateurs.php
@@ -50,7 +50,6 @@
                             <option value="">Séléctionnez un type</option>
                             <option value="1">Admin</option>
                             <option value="2">User</option>
-                            <option value="3">test</option>
 
                         </select>
                     </div>
