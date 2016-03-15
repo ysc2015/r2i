8e8d4249dbc00138c4378a39c6405153d4d0f147
rrahmouni <rrahmouni@90965971-89fd-4c9b-8651-a71ee073ffc4>
2016-03-15 15:14:36 +0000

    Failed commit: Default

--- assets/js/pages/r2i.projects_mod.js.php
+++ assets/js/pages/r2i.projects_mod.js.php
@@ -5,29 +5,29 @@ var API_URL = 'public/api/r2iApi.php';
 
 $(function () {
     function Update(){
-        var obj = new Object();
-        obj.project_id= project_id;
-        console.log(obj.project_id);
-        obj.info = new Object();
-        obj.info.ceation_date = $('#ceation_date').val();
-        console.log(obj.info.ceation_date);
-        obj.info.attribution_date = $('#attribution_date').val();
-        obj.info.project_name = $('#project_name').val();
-        obj.info.city = $('#city').val();
-        console.log(obj.info.city);
-        obj.info.plate_dept_code = $('#plate_dept_code').val();
-        obj.info.type_site_id = $('#type_site_id').val();
-        obj.info.size = $('#size').val();
-        obj.info.orig_site_state_id = $('#orig_site_state_id').val();
-        obj.info.orig_site_provision_date = $('#orig_site_provision_date').val();
-        console.log(obj);
+        var object = new Object();
+        object.project_id= project_id;
+        console.log(object.project_id);
+        object.info = new Object();
+        object.info.ceation_date = $('#ceation_date').val();
+        console.log(object.info.ceation_date);
+        object.info.attribution_date = $('#attribution_date').val();
+        object.info.project_name = $('#project_name').val();
+        object.info.city = $('#city').val();
+        console.log(object.info.city);
+        object.info.plate_dept_code = $('#plate_dept_code').val();
+        object.info.type_site_id = $('#type_site_id').val();
+        object.info.size = $('#size').val();
+        object.info.orig_site_state_id = $('#orig_site_state_id').val();
+        object.info.orig_site_provision_date = $('#orig_site_provision_date').val();
+        console.log(object);
         $.ajax({
 
             url: API_URL,
             type: 'POST',
             dataType: 'json',
             data: {
-                parameters: obj,
+                parameters: object,
                 method: 'update_project',
             },
             success: function (response) {
