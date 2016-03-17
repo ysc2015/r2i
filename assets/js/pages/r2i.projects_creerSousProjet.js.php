b53a9d2b83c089e963075c32b4ee15fcc28d3efb
iamara <iamara@90965971-89fd-4c9b-8651-a71ee073ffc4>
2016-03-17 17:28:34 +0000

    Failed commit: Default

--- assets/js/pages/r2i.projects_creerSousProjet.js.php
+++ assets/js/pages/r2i.projects_creerSousProjet.js.php
@@ -1,53 +1,80 @@
 <?php
 header("Content-type: application/javascript");
 ?>
-var API_URL = 'public/api/r2iApi.php';
 
-$(function () {
 
-    function creerSousProjet(){
-        var ob = new Object();
-        ob.project_id= project_id;
-        console.log(ob.project_id);
-        ob.info = new Object();
-        ob.info.res_type = $('#res_type').val();
-        console.log(ob.info.res_type);
-        ob.info.intervenantBE = $('#intervenantBE').val();
-        ob.info.plans = $('#plans').val();
-        ob.info.lineaireReseau = $('#lineaireReseau').val();
-        ob.info.controleDesPlans = $('#controleDesPlans').val();
-        console.log(ob.info.controleDesPlans);
-        ob.info.date_trans_plans = $('#date_trans_plans').val();
-        ob.info.entreprise = $('#entreprise').val();
-        ob.info.date_aiguillage = $('#date_aiguillage').val();
-        ob.info.Date_ret_Prev = $('#Date_ret_Prev').val();
-        ob.info.duree = $('#duree').val();
-        ob.info.controle_demarrage = $('#controle_demarrage').val();
-        ob.info.avancement = $('#avancement').val();
-        ob.info.controleParallele = $('#controleParallele').val();
-        ob.info.DateRetour = $('#DateRetour').val();
-        ob.info.EtatRetour = $('#EtatRetour').val();
-        ob.info.ControleApreTrv = $('#ControleApreTrv').val();
-        console.log(ob);
+var ProjectFormValidation = function() {
+    var API_URL = 'public/api/r2iApi.php';
+    var uploadObj = null;
+    var selectedFilesCounter = 0;
+    //get $_GET array
+    var get_$GET_Array = function() {
+        var $_GET = {};
+        if(document.location.toString().indexOf('?') !== -1) {
+            var query = document.location
+                .toString()
+                // get the query string
+                .replace(/^.*?\?/, '')
+                // and remove any existing hash string (thanks, @vrijdenker)
+                .replace(/#.*$/, '')
+                .split('&');
+
+            for(var i=0, l=query.length; i<l; i++) {
+                var aux = decodeURIComponent(query[i]).split('=');
+                $_GET[aux[0]] = aux[1];
+            }
+        }
+        return $_GET;
+    };
+
+    //call ajax -> api method
+    var executeMethod = function($methodname, $params) {
         $.ajax({
-
             url: API_URL,
             type: 'POST',
             dataType: 'json',
-            data: {
-                parameters: ob,
-                method: 'insert_sous_project',
-            },
+            data: ($params!=0?{parameters: $params,method : $methodname}:{method : $methodname}),
             success: function (response) {
-                if(response.status == 'success') {
-                    console.log("bien inserer");
-                }
+                console.log($methodname+':success');
+                console.log(response);
+
             },
             error: function (e) {
+                console.log($methodname+':error');
                 console.log(e.responseText);
             }
         });
-    }
-    $('#add-sous-project').click(creerSousProjet);
+    };
 
-});
+    var addUser = function() {
+        var $form = jQuery('.js-validation-bootstrap');
+        var $row = {}; // default row value;
+        // When the add project form is submitted
+        jQuery('#add-sous-project').on('click', function(){
+            console.log('Enregistrement sous projet');
+
+                $form.find('input,textarea,select').each(function () {
+                    $row[$(this).attr('name')] = $(this).val();
+                });
+                console.log(JSON.stringify($row));
+                //call ajax
+                executeMethod('insert_sous_project',JSON.stringify($row));
+
+        });
+    };
+    // Init page helpers
+
+
+
+    return {
+        init: function () {
+
+            // Update Event functionality
+            addUser();
+
+        }
+    };
+}();
+
+// Initialize when page loads
+jQuery(function(){ ProjectFormValidation.init(); });
