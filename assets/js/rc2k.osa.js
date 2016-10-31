4740ce637764e6baf7fb5fced57921b6d4970c32
mbourassi <mbourassi@90965971-89fd-4c9b-8651-a71ee073ffc4>
2016-10-31 13:59:41 +0000

    osa javascript lib

---
+++ assets/js/rc2k.osa.js
@@ -0,0 +1,38 @@
+
+(function(w){
+
+	if(!JQuery) throw "RC2K: Missing JQuery module";
+	w.rc2k.osa = {
+		ui : {
+			url : 'http://sd-83414.dedibox.fr/osa/rest/',
+			/*auth : function(account, callback){
+				$.ajax({'url':url + '/auth','method':'get','data':account}).done(callback);
+			},*/
+			tache : {
+				create:function(token){
+					window.open(rc2k.osa.ui.url + token + '/tache_create/' , '', 'location=no,menubar=no,resizable=no,scrollbars=no,status=no,titlebar=no,toolbar=no');
+				}
+			}
+		},
+		ws : {
+			url : 'http://sd-83414.dedibox.fr/osa/',
+			projet : {
+				create:function(obj, callback){
+					$.ajax({
+				        method: "POST",
+				        url: rc2k.osa.ws.url + "api/projet.php",
+				        data: {
+				            referant_projet: obj.ref,
+				            nom_projet: obj.prj,
+				            description_projet: obj.des,
+				            date_fin_projet: obj.dat,
+				            id_filiale: obj.fil,
+				            id_pole: obj.pol,
+				        }
+				    }).done(callback);	
+				}
+			}
+		}		
+	};
+
+})(window);
