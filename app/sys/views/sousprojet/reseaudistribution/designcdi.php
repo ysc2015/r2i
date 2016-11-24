b6a535bda48ff2e7346c98c3e4a977da8f291c9b
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2016-11-24 10:04:09 +0000

    Rejected commit: Default

--- app/sys/views/sousprojet/reseaudistribution/designcdi.php
+++ app/sys/views/sousprojet/reseaudistribution/designcdi.php
@@ -101,7 +101,8 @@
         calculetache_osa(typeetape,get("idsousprojet"),variable_etape,"designcdi_href","Design CDI/CAD: ");
 
         $("#id_sous_projet_distribution_design_btn_osa").click(function () {
-            appelscriptosa(typeetape,get("idsousprojet"),variable_etape);//1 = ide
+           // appelscriptosa(typeetape,get("idsousprojet"),variable_etape);//1 = ide
+            console.log("message affiche");
         });
         $("#id_sous_projet_distribution_design_list_tache").click(function () {
             liste_tache_osa(typeetape,get("idsousprojet"),variable_etape);//1 = ide
