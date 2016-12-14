dbcc79d0f70588ab1b08ec7a96b3518a3aa91ff8
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2016-12-14 12:28:41 +0000

    Rejected commit: Default

--- app/sys/api/osa/osa_api.php
+++ app/sys/api/osa/osa_api.php
@@ -46,10 +46,10 @@ class OsaApi
             ///
         }else{
             //print_r($res);
-            $resultat = json_decode($res, true) ;
+            $resultat = json_decode($res) ;
 
 
-            var_dump($resultat);
+            //var_dump($resultat);
 
             $tabencours= [];
             $tabtermine= [];
@@ -57,7 +57,7 @@ class OsaApi
             $encoure = 0;
             $termine = 0;
             $pasdereponse = '';
-             if(is_array($resultat) ){
+             if(!is_null($resultat) ){
                  if(count($resultat->ENCOURS)>0){
 
                      $bddresultat = $db->query("SELECT id_etape,count(*) FROM `sous_projet_taches_osa` where id_osa IN (".implode(',', array_values($resultat->ENCOURS)) .") and id_etape = '".$idetape."' and type_etape = '".$typeetape."'");
