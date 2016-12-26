3eb48e442d7f2369c7e0e4b4c6a16b8a45dac662
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2016-12-26 15:10:31 +0000

    Rejected commit: Default

--- app/sys/views/mail/edittemplate/edittemplate_block_begin.php
+++ app/sys/views/mail/edittemplate/edittemplate_block_begin.php
@@ -8,7 +8,7 @@
                 <button type="button" data-toggle="block-option" data-action="content_toggle"><i class="si si-arrow-up"></i></button>
             </li>
         </ul>
-        <h3 class="block-title">Liste utilisateurs (émails CC selon type)</h3>
+        <h3 class="block-title">Template mail notification</h3>
     </div>
     <div class="block-content">
         <div class="block" id="edittemplate_block_content">
