c5467d14bcfbe3c5574ee2f8f775eadebc015ee8
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-01-19 16:23:03 +0000

    Rejected commit: Default

--- app/sys/inc/utils.functions.php
+++ app/sys/inc/utils.functions.php
@@ -324,6 +324,19 @@ function openExcelFile($file) {
     }
 }
 
+function openExcelFile_write($file) {
+    try {
+        $inputFileType = PHPExcel_IOFactory::identify($file);
+        $objReader = PHPExcel_IOFactory::createReader($inputFileType);
+
+        $objPHPExcel = $objReader->load($file);
+        return $objPHPExcel;
+
+    } catch (Exception $ex) {
+        throw $ex;
+    }
+}
+
 function getHeader(PHPExcel_Worksheet $sheet) {
     $col = 0;
     $stop = false;
@@ -652,7 +665,6 @@ function loadExcelDEF_CABLE($db,$inputFileName,$idressource) {
                             break;
                         case 720:$capacite720_pec++;
                             break;
-
                     }
                 }
 
@@ -755,244 +767,7 @@ function loadExcelDEF_CABLE($db,$inputFileName,$idressource) {
     }
     return -1;
 }
-function calcule_devis_from_pds($db,$inputFileName,$idressource) {
-$RFO_01_01 = 0;
-$RFO_01_03 = 0;
-$RFO_01_05 = 0;
-$RFO_01_07 = 0;
-$RFO_01_09 = 0;
-$RFO_01_11 = 0;
-$RFO_01_13 = 0;
 
-    $tabreturn = [];
-    try {
-        $excel = openExcelFile($inputFileName);
-        $stop = microtime(true);
-        // echo "Contenu openExcelFile généré en ".number_format(($stop-$start), 3)." seconde(s)<br /><br />";
-
-        $arr = $excel->getSheetNames();
-        $stop = microtime(true);
-        // echo "Contenu getSheetNames généré en ".number_format(($stop-$start), 3)." seconde(s)<br /><br />";
-
-        $tab=array();
-        $i=0;$j=0;$valtrouve = 0;
-        $countcap = [];$cap = 0;
-        $countg1=0 ;
-        $NBSOUD = 0;
-        foreach ($arr as $key => $value) {
-
-            $sheet = $excel->getSheetByName($value);
-            $header = getHeader($sheet);
-            $stop = microtime(true);
-
-            if ($value=="DEF_BPE"){
-                //$db->query("TRUNCATE testDEF_BPE_CABLE");
-
-                $row = 4;
-                $max = count($header);
-                /*while($read = getLine($sheet,$row,100)) {
-
-                    if( strstr($read[0], "PDB") && $read[7]=="OUI") {
-
-                     //   $db->query("insert into testDEF_BPE_CABLE (id,nom,passage,capacite,NB_FO_SOUD) values(NULL,'" . $read[0] . "','" . $read[2] . "','" . $read[12] . "','" . $read[6] . "')") or die();
-                        $i++;
-                    }
-                    $row++;
-                }*/
-                $nom_feuille_pdb = "";
-                while($read = getLine($sheet,$row,100)) {
-
-                    if( strstr($read[0], "PDB") && $read[10] > 0 ) {
-                        $nom_feuille_pdb = substr_replace($read[0],'_',16)  ;
-                        echo $nom_feuille_pdb;
-                        die();
-                       // $db->query("insert into testDEF_BPE_CABLE2 (id,nom,passage,capacite,NB_FO_SOUD) values(NULL,'" . $read[0] . "','" . $read[2] . "','" . $read[12] . "','" . $read[6] . "')") or die();
-                        $i++;
-                    }
-                    $row++;
-                }
-
-
-                $row--;
-            }
-           /*
-            elseif(strstr($value,"CDI")){
-                $g1 = $sheet->getCell("G1")->getCalculatedValue();
-                $countg1 = ($countg1 + intval($g1));
-
-            }
-            elseif (strstr($value,"CTR")) {
-
-                $row = 9;
-                //$max = count($header);
-                $i=0;
-                $loop = true;
-
-                while($loop) {
-                    $read = getLine($sheet,$row,15);
-                    if ($i>5){
-                        $loop = false;
-                        break;
-                    }
-                    if($read==null) {
-                        $row++;
-                        $i++;
-                        continue;
-                    }
-                    $i = 0;
-                    if( ( $read[7]== "E") && (strstr($read[13],"CDI"))) {
-                        $NBSOUD++;
-                    }
-                    $row++;
-                }
-
-
-            }
-*/
-        }
-        die();
-        $tabext = [];
-        $tabfen = [];
-        $tabrac = [];
-        $nbtubtab = [];
-
-
-        $i=0;
-
-
-        $reqpercapacite = $db->query("select count(nom),capacite from testDEF_CABLE group by capacite order by capacite ");
-        while($percapacite= $reqpercapacite->fetch()){
-            $tabext[$i][0] = $percapacite[0] * 2 ;
-            $tabext[$i][1] = $percapacite[1];
-            $i++;
-
-        }
-
-        $reqpercapacite = $db->query("select count(nom),capacite from testDEF_BPE_CABLE group by capacite order by capacite ");
-        while($percapacite = $reqpercapacite->fetch()){
-            $tabfen[$i][0] = $percapacite[0];
-            $tabfen[$i][1] = $percapacite[1];
-            $i++;
-
-        }
-        $totalnbtub = 0;
-
-        $reqpercapacite = $db->query("select nom,capacite,NB_FO_SOUD from testDEF_BPE_CABLE where nom like 'CDI%' and passage like 'OUI' ");
-        while($percapacite = $reqpercapacite->fetch()){
-            $nbtubtab[$i][0] = $percapacite[0];
-            $nbtubtab[$i][1] = $percapacite[2];
-            $nbtubtab[$i][2] = ceil($percapacite[2]/12);
-            $totalnbtub = $totalnbtub + ceil($percapacite[2]/12);
-            $i++;
-
-        }
-        $nbtub = $totalnbtub;
-
-        $TABEXT_432 = 0; $TABEXT_288= 0; $TABEXT_144= 0; $TABEXT_72= 0; $TABEXT_48= 0; $TABEXT_24= 0;
-        $TABRAC_720=0; $TABRAC_432 = 0; $TABRAC_288= 0; $TABRAC_144= 0; $TABRAC_72= 0; $TABRAC_48= 0; $TABRAC_24= 0;
-        $TABFEN_432 = 0; $TABFEN_288= 0; $TABFEN_144= 0; $TABFEN_72= 0; $TABFEN_48= 0; $TABFEN_24= 0;
-
-        foreach ($tabext as $item){
-            switch ($item[1]){
-                case 24 : $TABEXT_24 = $item[0];break;
-                case 48 :  $TABEXT_48 = $item[0];break;
-                case 72 :  $TABEXT_72 = $item[0];break;
-                case 144 :  $TABEXT_144 = $item[0];break;
-                case 288 :  $TABEXT_288 = $item[0];break;
-                case 432 :   $TABEXT_432 = $item[0];break;
-            }
-            $insred = 0;
-            foreach ($tabfen as $value){
-
-                if($item[1]==$value[1]){
-                    $tabrac[$j][1] = $item[1];
-                    $tabrac[$j][0] = $item[0] - ($value[0]*2);
-                    $insred = 1;
-                    $j++;
-                }
-
-            }
-            // valeur qui ne se trouve plus sur $tabfen
-
-            if($insred==0){
-                $tabrac[$j][1] = $item[1];
-                $tabrac[$j][0] = $item[0];
-
-            }
-
-
-        }
-        $NBSOUD = $countg1 + $NBSOUD;
-        $tabreturn[0] = $tabext;
-        $tabreturn[1] = $tabfen;
-        $tabreturn[2] = $tabrac;
-        $tabreturn[3] = $nbtub;
-        $tabreturn[4] = $NBSOUD ;
-
-
-
-        foreach ($tabrac as $item){
-            switch ($item[1]){
-                case 24 : $TABRAC_24 = $item[0];break;
-                case 48 : $TABRAC_48 = $item[0];break;
-                case 72 :   $TABRAC_72 = $item[0];break;
-                case 144 :  $TABRAC_144 = $item[0];break;
-                case 288 :  $TABRAC_288 = $item[0];break;
-                case 432 :  $TABRAC_720 = $item[0];break;
-            }
-        }
-        foreach ($tabfen as $item){
-            switch ($item[1]){
-                case 24 :  $TABFEN_24 = $item[0];break;
-                case 48 :  $TABFEN_48 = $item[0];break;
-                case 72 :   $TABFEN_72 = $item[0];break;
-                case 144 :  $TABFEN_144 = $item[0];break;
-                case 288 :   $TABFEN_288 = $item[0];break;
-                case 432 :   $TABFEN_432 = $item[0];break;
-            }
-        }
-
-
-        $stm = $db->prepare("INSERT INTO `detaildevis` (`iddevis`,id_ressource, `TABEXT_432`, `TABEXT_288`, `TABEXT_144`, `TABEXT_72`, `TABEXT_48`, `TABEXT_24`,
-`TABRAC_720`, `TABRAC_432`, `TABRAC_288`, `TABRAC_144`, `TABRAC_72`, `TABRAC_48`, `TABRAC_24`,
- `TABFEN_432`, `TABFEN_288`, `TABFEN_144`, `TABFEN_72`,  `TABFEN_48`, `TABFEN_24`,`NBTUB`, `NBSOUD`, `dateinsert`) 
- VALUES (NULL,  :id_ressource,  :TABEXT_432, :TABEXT_288, :TABEXT_144,  :TABEXT_72, :TABEXT_48, :TABEXT_24,
-   :TABRAC_720, :TABRAC_432,    :TABRAC_288, :TABRAC_144, :TABRAC_72,   :TABRAC_48, :TABRAC_24,
-   :TABFEN_432, :TABFEN_288,    :TABFEN_144, :TABFEN_72,  :TABFEN_48,   :TABFEN_24, :nbtub, :NBSOUD,:dateaction )");
-
-        $stm->bindValue(':id_ressource',$idressource);
-        $stm->bindValue(':TABEXT_432',$TABEXT_432);
-        $stm->bindValue(':TABEXT_288',$TABEXT_288);
-        $stm->bindValue(':TABEXT_144',$TABEXT_144);
-        $stm->bindValue(':TABEXT_72',$TABEXT_72);
-        $stm->bindValue(':TABEXT_48',$TABEXT_48);
-        $stm->bindValue(':TABEXT_24',$TABEXT_24);
-        $stm->bindValue(':TABRAC_720',$TABRAC_720);
-        $stm->bindValue(':TABRAC_432',$TABRAC_432);
-        $stm->bindValue(':TABRAC_288',$TABRAC_288);
-        $stm->bindValue(':TABRAC_144',$TABRAC_144);
-        $stm->bindValue(':TABRAC_72',$TABRAC_72);
-        $stm->bindValue(':TABRAC_48',$TABRAC_48);
-        $stm->bindValue(':TABRAC_24',$TABRAC_24);
-        $stm->bindValue(':TABFEN_432',$TABFEN_432);
-        $stm->bindValue(':TABFEN_288',$TABFEN_288);
-        $stm->bindValue(':TABFEN_144',$TABFEN_144);
-        $stm->bindValue(':TABFEN_72',$TABFEN_72);
-        $stm->bindValue(':TABFEN_48',$TABFEN_48);
-        $stm->bindValue(':TABFEN_24',$TABFEN_24);
-        $stm->bindValue(':NBSOUD',$NBSOUD);
-        $stm->bindValue(':nbtub',$nbtub);
-        $stm->bindValue(':dateaction',date('Y-m-d G:i:s'));
-        $stm->execute();
-
-
-        return json_encode($tabreturn);
-
-    } catch (Exception $e) {
-        die ('Error loading file "' . pathinfo($inputFileName, PATHINFO_BASENAME) . '": ' . $e->getMessage());
-    }
-    return -1;
-}
 function parse_loadExcelDEF_CABLE($db,$inputFileName,$templateFileName,$id) {
 
     $sousProjet = NULL;
@@ -1114,7 +889,7 @@ function parse_loadExcelDEF_CABLE($db,$inputFileName,$templateFileName,$id) {
         $stm->execute();
         $row = $stm->fetch(PDO::FETCH_OBJ);
 
-        $Bordereaux = openExcelFile($templateFileName);
+        $Bordereaux = openExcelFile_write($templateFileName);
         $sheetbordereaux = $Bordereaux->getSheetByName("LOT7_FO");
 
 
@@ -1543,7 +1318,7 @@ function parse_DEF_BPE_EBM($db,$inputFileName,$templateFileName,$id) {
         $stm->execute();
         $row = $stm->fetch(PDO::FETCH_OBJ);
 
-        $Bordereaux = openExcelFile($templateFileName);
+        $Bordereaux = openExcelFile_write($templateFileName);
         $sheetbordereaux = $Bordereaux->getSheetByName("EBM_PON_HRZ");
 
         $sheetbordereaux->getCell("L8")->setValue($row->L8);
