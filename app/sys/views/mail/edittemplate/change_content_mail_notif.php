4e89d268e98aaec32c2d6f1eaff178f7e2d35f29
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-01-30 14:05:53 +0000

    Rejected commit: Default

--- app/sys/views/mail/edittemplate/change_content_mail_notif.php
+++ app/sys/views/mail/edittemplate/change_content_mail_notif.php
@@ -10,13 +10,28 @@ include_once "../../../language/fr/default.php";
 
 extract($_GET);
 if(isset($contenthtml)){
-    $stm_mail_content_notif = $db->prepare("update mail_notification_template set  template = :contenthtml, object = :object where type = :type");
 
-    $stm_mail_content_notif->bindParam(':contenthtml',$contenthtml);
-    $stm_mail_content_notif->bindParam(':object',$object);
-    $stm_mail_content_notif->bindParam(':type',$type);
-    if($stm_mail_content_notif->execute()) echo "OK";
-    else echo "NOK";
+    $stm_mail_content_exist = $db->prepare("select * from  mail_notification_template  where type = :type");
+    $stm_mail_content_exist->bindParam(':type',$type);
+    $stm_mail_content_exist->execute();
+    if($stm_mail_content_exist->rowCount()>0){
+        $stm_mail_content_notif = $db->prepare("update mail_notification_template set  template = :contenthtml, object = :object where type = :type");
+
+        $stm_mail_content_notif->bindParam(':contenthtml',$contenthtml);
+        $stm_mail_content_notif->bindParam(':object',$object);
+        $stm_mail_content_notif->bindParam(':type',$type);
+        if($stm_mail_content_notif->execute()) echo " update OK";
+        else echo "NOK";
+    }else{
+        $stm_mail_content_notif = $db->prepare("insert into mail_notification_template (id_mail_notification,template,type,object) values(NULL,:contenthtml,:type,:object)");
+
+        $stm_mail_content_notif->bindParam(':contenthtml',$contenthtml);
+        $stm_mail_content_notif->bindParam(':object',$object);
+        $stm_mail_content_notif->bindParam(':type',$type);
+        if($stm_mail_content_notif->execute()) echo "new insert OK";
+        else echo "NOK";
+    }
+
 
 
 }else{
