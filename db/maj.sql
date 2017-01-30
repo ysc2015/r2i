ed90856fa8c2b536d87d02edcacdbbc2bf511c28
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-01-30 11:04:20 +0000

    Rejected commit: Default

--- db/maj.sql
+++ db/maj.sql
@@ -7,3 +7,10 @@ ALTER TABLE `sous_projet_transport_commande_fin_travaux` CHANGE `ok_ft` `go_ft` 
 ALTER TABLE `sous_projet_distribution_commande_fin_travaux` ADD `date_depot_cmd` DATE NULL DEFAULT NULL AFTER `date_transmission_tfx`;
 
 ALTER TABLE `sous_projet_transport_commande_fin_travaux` ADD `date_depot_cmd` DATE NULL DEFAULT NULL AFTER `date_transmission_tfx`;
+
+INSERT INTO `type_notification` (`id_type_notification`, `lib_type_notification`) VALUES
+(6, 'validation des retours STT dans l''OT'),
+(7, 'validation d''une étape'),
+(8, 'ajout d''un point bloquant'),
+(9, 'Ajout d''un PBC (cron toutes les heures)'),
+(10, 'Réponse à un PBC ');
