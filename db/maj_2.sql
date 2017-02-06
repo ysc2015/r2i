2c58cc81ba647eb659ad83e84a763c3b33af0ff6
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-02-06 17:02:10 +0000

    Rejected commit: Default

--- db/maj_2.sql
+++ db/maj_2.sql
@@ -6,4 +6,4 @@ ALTER TABLE `blq_pbc` ADD `flag` INT NOT NULL DEFAULT '0' AFTER `date_action`;
 ALTER TABLE  `ordre_de_travail` ADD  `date_creation` DATE NULL DEFAULT NULL ;
 
 
-UPDATE `ordre_de_travail` SET `date_creation` = '2017-02-06' WHERE 1;
+update `ordre_de_travail` set date_creation = date_debut WHERE 1;
