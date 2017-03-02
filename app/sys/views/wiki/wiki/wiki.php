6346db923231a5b1d88e96a35f4928cde69b4c8e
rrahmouni <rrahmouni@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-03-02 15:21:31 +0000

    wiki v1.1

--- app/sys/views/wiki/wiki/wiki.php
+++ app/sys/views/wiki/wiki/wiki.php
@@ -9,7 +9,6 @@ ul#files li img{ max-width:180px; max-height:150px; }
 .success{ background:#99f099; border:1px solid #339933; }
 .error{ background:#f0c6c3; border:1px solid #cc6622; }
 
-
 </style>	
 
 <?php if (! isset ( $_GET ['action'])) {
