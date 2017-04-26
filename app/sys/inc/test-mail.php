f01f89625eed4f784748fcd9ad2c2e8eb0a0a8b7
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-04-26 15:15:30 +0000

    Rejected commit: Default

---
+++ app/sys/inc/test-mail.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fadil
+ * Date: 26/04/17
+ * Time: 04:13 م
+ */
+
+
+
+require_once "mail.notifier.class.php";
+
+if(@MailNotifier::sendMail("my object","du text",["fadelghani@gmail.com","fadelghani@rc2k.fr"],array())) {
+    $message[] = "Mail envoyé !";
+} else {
+    $message[] = "Mail non envoyé !";
+    $err++;
+}
+
