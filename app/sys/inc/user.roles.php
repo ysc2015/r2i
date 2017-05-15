21e9e931c264652692952cce7268d5505e674dbf
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-05-15 10:11:27 +0000

    Rejected commit: Default

--- app/sys/inc/user.roles.php
+++ app/sys/inc/user.roles.php
@@ -138,7 +138,7 @@ class adm extends baseUser {
             array(
                 "dashboard1" => array("index"),
                 "activitevpi" => array("gestionplans","gestiontrvx","gestionpbcpbt","gestionfinprod"),
-                "activitebei" => array("statbei1","designreseau")/*,
+                "activitebei" => array("statbei1","designreseau","fin_design_hors_delai","plan_aiguillage_attributions","plan_aiguillage_non_uploades")/*,
                 "productiongenerale" => array("index"),*/
             )
         );
