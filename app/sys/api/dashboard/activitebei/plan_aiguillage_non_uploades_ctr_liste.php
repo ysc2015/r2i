8a50c8bad5be226b4f36696c2a44ad3746bdecea
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-05-15 10:11:27 +0000

    Rejected commit: Default

---
+++ app/sys/api/dashboard/activitebei/plan_aiguillage_non_uploades_ctr_liste.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fadil
+ * Date: 10/05/17
+ * Time: 04:41 م
+ */
+extract($_POST);
+//Liste des CTR pour lesquels le design est terminé mais pour lesquels les plans d’aiguillage ne sont pas « Contrôlés OK » ou « Lien vers les plans » est vide.
+$table = array("sous_projet as t1","projet as t2","nro as t3","ordre_de_travail as t4","`sous_projet_transport_design` as t5","`sous_projet_transport_aiguillage` as t9" );
+$columns = array(
+    array( "db" => "t1.id_sous_projet", "dt" => 'id_sous_projet' ),
+    array( "db" => "t1.id_projet", "dt" => 'id_projet' ),
+    array( "db" => "t2.projet_nom", "dt" => 'projet_nom' ),
+    array( "db" => "t1.dep", "dt" => 'dep' ),
+    array( "db" => "t1.ville", "dt" => 'ville' ),
+    array( "db" => "t1.plaque", "dt" => 'plaque' ),
+    array( "db" => "t1.zone", "dt" => 'zone' ),
+    array( "db" => "t3.lib_nro", "dt" => 'lib_nro' ),
+    array( "db" => "t3.id_nro", "dt" => 'id_nro' ),
+    array( "db" => "t4.date_debut", "dt" => 'date_debut' ),
+    array( "db" => "t4.date_fin", "dt" => 'date_fin' ),
+    array( "db" => "t7.nom_utilisateur", "dt" => 'nom_utilisateur' ),
+    array( "db" => "t7.prenom_utilisateur", "dt" => 'prenom_utilisateur' )
+);
+$condition  = " t1.id_projet=t2.id_projet AND t2.id_nro=t3.id_nro ";
+$condition .= " AND t1.id_sous_projet=t4.id_sous_projet ";
+$condition .= " AND t1.id_sous_projet=t5.id_sous_projet ";
+$condition .= " AND t1.id_sous_projet=t9.id_sous_projet ";
+$condition .= " AND t5.ok = 1 ";
+$condition .= " AND t9.plans = 3 ";
+$condition .= " AND (t9.controle_plans != 2 || t9.lien_plans = '' ) ";
+
+$condition .= " group by t1.id_sous_projet  ";
+
+
+switch($connectedProfil->profil->profil->shortlib) {
+
+    /*case "cdp" :
+        $condition .=" AND t2.id_chef_projet = ".$connectedProfil->profil->id_utilisateur;
+        break;*/
+
+    case "pci" :
+    case "bei" :
+        $arr = array(-1);
+        $stm_bei_pci = $db->prepare("select id_nro from nro_utilisateur where id_utilisateur = ".$connectedProfil->profil->id_utilisateur);
+        $stm_bei_pci->execute();
+        $nros = $stm_bei_pci->fetchAll();
+        foreach($nros as $nro) {
+            $arr[] = $nro['id_nro'];
+        }
+
+        $condition .=" AND t2.id_nro IN ( ".implode(",",$arr).")";
+        break;
+
+    case "vpi" :
+        $arr = array(-1);
+        $stm = $db->prepare("select id_nro from nro where id_utilisateur = ".$connectedProfil->profil->id_utilisateur);
+        $stm->execute();
+        $nros = $stm->fetchAll();
+        foreach($nros as $nro) {
+            $arr[] = $nro['id_nro'];
+        }
+
+        $condition .=" AND t2.id_nro IN ( ".implode(",",$arr).")";
+        break;
+
+    default : break;
+}
+
+$left = " LEFT JOIN `nro_utilisateur` as t6 on t6.id_nro =  t3.id_nro ";
+$left .= " LEFT JOIN utilisateur as t7 on t7.id_utilisateur = t6.id_utilisateur ";
+$left .= " LEFT JOIN profil_utilisateur as t8 on  t8.id_profil_utilisateur=t7.id_profil_utilisateur and t8.id_profil_utilisateur=4 ";
+
+echo json_encode(SSP::simpleJoin($_GET,$db,$table,"id_sous_projet",$columns,$condition,$left));
+?>
---
+++ app/sys/views/dashboard/activitebei/plan_aiguillage_attributions.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fadil
+ * Date: 11/05/17
+ * Time: 01:14 م
+ */
+
+?>
+<div class="block block-themed" id="designreseau_block">
+    <div class="block-header bg-primary-light">
+        <ul class="block-options">
+            <li>
+                <button id="plan_aiguillage_attributions_block_refresh" type="button" data-toggle="block-option" data-action="refresh_toggle" data-action-mode="demo"><i class="si si-refresh"></i></button>
+            </li>
+        </ul>
+        <h3 class="block-title">Préparations Plans d’aiguillage</h3>
+    </div>
+    <div class="block-content">
+        <!-- Page Content -->
+        <div class="content">
+            <div class="row">
+                <div class="block">
+                    <div class="block-header">
+                        <h3 class="block-title">Attributions            </h3>
+                    </div>
+                    <div class="block-content bg-info-light">
+                        <table id="plan_aiguillage_attributions_ctr_table" class="table table-bordered table-striped js-dataTable-full" width="100%">
+                            <thead>
+                            <tr><th colspan="3">Réseau de Transport</th></tr>
+                            <tr>
+                                <th>Code Sous-projet</th>
+                                <th>Date fin Positionnement Adresse</th>
+                                <th>BEI du NRO</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                            <tfoot>
+
+                            <tr>
+                                <th>Code Sous-projet</th>
+                                <th>Date fin Positionnement Adresse</th>
+                                <th>BEI du NRO</th>
+                            </tr>
+                            <tr><th colspan="3">Réseau de Transport</th></tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <br />
+                    <br />
+                    <div class="block-content bg-info-light">
+                        <table id="plan_aiguillage_attributions_cdi_table" class="table table-bordered table-striped js-dataTable-full" width="100%">
+                            <thead>
+                            <tr><th colspan="3">Réseau de Distribution</th></tr>
+                            <tr>
+                                <th>Code Sous-projet</th>
+                                <th>Date fin Positionnement Adresse</th>
+                                <th>BEI du NRO</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                            <tfoot>
+
+                            <tr>
+                                <th>Code Sous-projet</th>
+                                <th>Date fin Positionnement Adresse</th>
+                                <th>BEI du NRO</th>
+                            </tr>
+                            <tr><th colspan="3">Réseau de Transport</th></tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- END Page Content -->
+    </div>
+</div>
+<script>
+    var plan_aiguillage_attributions_ctr_dt;
+    var plan_aiguillage_attributions_cdi_dt;
+    $(function () {
+        // Init page plugins & helpers
+    });
+
+    $(document).ready(function() {
+
+        plan_aiguillage_attributions_ctr_dt = $('#plan_aiguillage_attributions_ctr_table').on('preXhr.dt', function ( e, settings, data ) {
+            $('#plan_aiguillage_attributions_block_refresh').addClass('block-opt-refresh');
+        }).DataTable( {
+            "language": {
+                "url": "assets/js/plugins/datatables/French.json"
+            },
+            "autoWidth": false,
+            "processing": true,
+            "serverSide": true,
+            "ajax": {
+                "url": 'api/dashboard/activitebei/plan_aiguillage_attributions_ctr_liste.php'
+            },
+            "columns": [
+                { "data": "bei_nro" },
+                { "data": "date_fin" },
+                { "data": "bei_nro" }
+            ],
+            "columnDefs": [
+
+
+                {
+                    "targets": 2,
+                    "data": "lib_nro",
+                    "render": function ( data, type, full, meta ) {
+                        if(full.nom_utilisateur !=null && full.prenom_utilisateur != null )
+                            return full.nom_utilisateur + '-' + full.prenom_utilisateur;
+                        else return "n/d";
+                    }
+                },
+
+                {
+                    "targets": 0,
+                    orderData: [ 0, 1 ],
+                    "data": "lib_nro",
+                    "render": function ( data, type, full, meta ) {
+                        if(type == "display"){
+                            return  '<a href="?page=sousprojet&idsousprojet='+full.id_sous_projet+'">'+full.lib_nro + '-' + full.zone+'</a>';
+                        }
+
+                        return full.lib_nro + '-' + full.zone;
+                    }
+                }
+            ],
+            "order": [[0, 'desc']]
+            ,
+            "drawCallback": function( settings ) {
+                $('#plan_aiguillage_attributions_block_refresh').removeClass('block-opt-refresh');
+            }
+        } );
+        plan_aiguillage_attributions_cdi_dt = $('#plan_aiguillage_attributions_cdi_table').on('preXhr.dt', function ( e, settings, data ) {
+            $('#plan_aiguillage_attributions_block_refresh').addClass('block-opt-refresh');
+        }).DataTable( {
+            "language": {
+                "url": "assets/js/plugins/datatables/French.json"
+            },
+            "autoWidth": false,
+            "processing": true,
+            "serverSide": true,
+            "ajax": {
+                "url": 'api/dashboard/activitebei/plan_aiguillage_attributions_cdi_liste.php'
+            },
+            "columns": [
+                { "data": "bei_nro" },
+                { "data": "date_fin" },
+                { "data": "bei_nro" }
+            ],
+            "columnDefs": [
+
+
+                {
+                    "targets": 2,
+                    "data": "lib_nro",
+                    "render": function ( data, type, full, meta ) {
+                        if(full.nom_utilisateur !=null && full.prenom_utilisateur != null )
+                            return full.nom_utilisateur + '-' + full.prenom_utilisateur;
+                        else return "n/d";
+                    }
+                },
+
+                {
+                    "targets": 0,
+                    orderData: [ 0, 1 ],
+                    "data": "lib_nro",
+                    "render": function ( data, type, full, meta ) {
+                        if(type == "display"){
+                            return  '<a href="?page=sousprojet&idsousprojet='+full.id_sous_projet+'">'+full.lib_nro + '-' + full.zone+'</a>';
+                        }
+
+                        return full.lib_nro + '-' + full.zone;
+                    }
+                }
+            ],
+            "order": [[0, 'desc']]
+            ,
+            "drawCallback": function( settings ) {
+                $('#plan_aiguillage_attributions_block_refresh').removeClass('block-opt-refresh');
+            }
+        } );
+
+    } );
+</script>
