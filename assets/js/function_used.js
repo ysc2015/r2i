8d087214691fade50b8e7c839aaeced6e2f7422c
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-05-16 14:42:26 +0000

    Rejected commit: Default

--- assets/js/function_used.js
+++ assets/js/function_used.js
@@ -3,11 +3,11 @@
  */
 var weekday = new Array(7);
 weekday[0] =  "sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
+weekday[1] = "monday";
+weekday[2] = "tuesday";
+weekday[3] = "wednesday";
+weekday[4] = "thursday";
+weekday[5] = "friday";
 weekday[6] = "saturday";
 
 
