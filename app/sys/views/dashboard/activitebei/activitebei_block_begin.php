250b8a1d32b521053e8c988c2e8edf987632d00a
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-05-18 14:55:14 +0000

    Rejected commit: Default

--- app/sys/views/dashboard/activitebei/activitebei_block_begin.php
+++ app/sys/views/dashboard/activitebei/activitebei_block_begin.php
@@ -5,7 +5,7 @@
                 <button id="activitebei_block_refresh" type="button" data-toggle="block-option" data-action="refresh_toggle" data-action-mode="demo"><i class="si si-refresh"></i></button>
             </li>
             <li>
-                <button type="button" data-toggle="block-option" data-action="content_toggle"><i class="si si-arrow-up"></i></button>
+                <button id="activitebei_block_reduce" type="button" data-toggle="block-option" data-action="content_toggle"><i class="si si-arrow-up"></i></button>
             </li>
         </ul>
         <h3 class="block-title">Activité BEI Général</h3>
