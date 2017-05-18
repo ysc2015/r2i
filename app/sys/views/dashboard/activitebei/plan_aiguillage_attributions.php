1026c43468eb9ebd5c5a13ca682dc27f9b5c69a0
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-05-18 14:55:27 +0000

    Rejected commit: Default

--- app/sys/views/dashboard/activitebei/plan_aiguillage_attributions.php
+++ app/sys/views/dashboard/activitebei/plan_aiguillage_attributions.php
@@ -7,11 +7,14 @@
  */
 
 ?>
-<div class="block block-themed" id="designreseau_block">
+<div class="block block-themed" id="plan_aiguillage_attributions_block">
     <div class="block-header bg-primary-light">
         <ul class="block-options">
             <li>
                 <button id="plan_aiguillage_attributions_block_refresh" type="button" data-toggle="block-option" data-action="refresh_toggle" data-action-mode="demo"><i class="si si-refresh"></i></button>
+            </li>
+            <li>
+                <button id="plan_aiguillage_attributions_block_reduce" type="button" data-toggle="block-option" data-action="content_toggle" data-action-mode="demo"><i class="si si-arrow-up"></i></button>
             </li>
         </ul>
         <h3 class="block-title">Préparations Plans d’aiguillage</h3>
