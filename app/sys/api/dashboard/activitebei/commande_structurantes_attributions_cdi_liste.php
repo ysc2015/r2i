98a2d61596bdd77423fac0e42fc0679062066493
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-05-22 10:11:23 +0000

    Rejected commit: Default

---
+++ app/sys/api/dashboard/activitebei/commande_structurantes_attributions_cdi_liste.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fadil
+ * Date: 10/05/17
+ * Time: 04:41 م
+ */
+extract($_POST);
+
+ $tat = get_date_to_compare_ouvre(date('Y-m-d'),2);
+$date_plus_2_jours = $tat->format('Y-m-d');
+
+
+$table = array("retour_presta_sans_intervenant_cdi as t1");
+$columns = array(
+    array( "db" => "t1.lib_nro", "dt" => 'lib_nro' ),
+    array( "db" => "t1.zone", "dt" => 'zone' ),
+    array( "db" => "t1.idsp", "dt" => 'idsp' ),
+    array( "db" => "t1.date_r_p_j_p2", "dt" => 'date_r_p_j_p2' ),
+    array( "db" => "t1.etape", "dt" => 'etape' ),
+    array( "db" => "t1.type", "dt" => 'type' ),
+    array( "db" => "t1.nom_utilisateur", "dt" => 'nom_utilisateur' ),
+    array( "db" => "t1.prenom_utilisateur", "dt" => 'prenom_utilisateur' )
+);
+
+
+$condition = " 1 group by t1.idsp ";
+
+
+echo json_encode(@SSP::simpleJoin($_POST,$db,$table,"idsp",$columns,$condition,$left));
+?>
