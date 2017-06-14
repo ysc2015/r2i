1eace8e6677e32f77131591f354952209ea84bfd
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-06-14 14:20:40 +0000

    Rejected commit: Default

--- app/sys/views/dashboard/activitebei/commande_fin_travaux.php
+++ app/sys/views/dashboard/activitebei/commande_fin_travaux.php
@@ -72,6 +72,7 @@
         </div>
         <!-- END Page Content -->
     </div>
+    <!-- Fin de Commande de fin de travaux Hors Délais -->
     <div class="block-content">
         <!-- Page Content -->
         <div class="content" id="commande_fin_travaux_hors_delai_block_content">
@@ -134,6 +135,133 @@
         </div>
         <!-- END Page Content -->
     </div>
+    <!--saisie reference commande fin travaux-->
+    <div class="block-content">
+        <!-- Page Content -->
+        <div class="content" id="saisie_reference_commande_fin_travaux_block_content">
+            <div class="row">
+                <div class="block">
+                    <div class="block-header">
+                        <h3 class="block-title">Saisie de la référence de Commande fin de Travaux</h3>
+                    </div>
+                    <div class="block-content bg-info-light">
+                        <table id="saisie_reference_commande_fin_travaux_ctr_table1" class="table table-bordered table-striped js-dataTable-full" width="100%">
+                            <thead>
+                            <tr><th colspan="3">Réseau de Transport</th></tr>
+                            <tr>
+                                <th>Code Sous-projet</th>
+                                <th>Date calculées J+5 (jours ouvrés) à partir de la date de Transmission TFX</th>
+                                <th>BEI du NRO</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                            <tfoot>
+
+                            <tr>
+                                <th>Code Sous-projet</th>
+                                <th>Date calculées J+5 (jours ouvrés) à partir de la date de Transmission TFX</th>
+                                <th>BEI du NRO</th>
+                            </tr>
+                            <tr><th colspan="3">Réseau de Transport</th></tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <br />
+                    <br />
+                    <div class="block-content bg-info-light">
+                        <table id="saisie_reference_commande_fin_travaux_cdi_table1" class="table table-bordered table-striped js-dataTable-full" width="100%">
+                            <thead>
+                            <tr><th colspan="3">Réseau de Distribution</th></tr>
+                            <tr>
+                                <th>Code Sous-projet</th>
+                                <th>Date calculées J+6 (jours ouvrés) à partir de l’attribution de la Fin de travaux</th>
+                                <th>BEI du NRO</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                            <tfoot>
+
+                            <tr>
+                                <th>Code Sous-projet</th>
+                                <th>Date calculées J+6 (jours ouvrés) à partir de l’attribution de la Fin de travaux</th>
+                                <th>BEI du NRO</th>
+                            </tr>
+                            <tr><th colspan="3">Réseau de Distribution</th></tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- END Page Content -->
+    </div>
+    <!--commande refusé commande fin travaux-->
+    <div class="block-content">
+        <!-- Page Content -->
+        <div class="content" id="commande_refuse_commande_fin_travaux_block_content">
+            <div class="row">
+                <div class="block">
+                    <div class="block-header">
+                        <h3 class="block-title">Commandes Refusées</h3>
+                    </div>
+                    <div class="block-content bg-info-light">
+                        <table id="commande_refuse_commande_fin_travaux_ctr_table1" class="table table-bordered table-striped js-dataTable-full" width="100%">
+                            <thead>
+                            <tr><th colspan="3">Réseau de Transport</th></tr>
+                            <tr>
+                                <th>Code Sous-projet</th>
+                                <th>Date du Refus </th>
+                                <th>BEI du NRO</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                            <tfoot>
+
+                            <tr>
+                                <th>Code Sous-projet</th>
+                                <th>Date du Refus</th>
+                                <th>BEI du NRO</th>
+                            </tr>
+                            <tr><th colspan="3">Réseau de Transport</th></tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <br />
+                    <br />
+                    <div class="block-content bg-info-light">
+                        <table id="commande_refuse_commande_fin_travaux_cdi_table1" class="table table-bordered table-striped js-dataTable-full" width="100%">
+                            <thead>
+                            <tr><th colspan="3">Réseau de Distribution</th></tr>
+                            <tr>
+                                <th>Code Sous-projet</th>
+                                <th>Date du Refus</th>
+                                <th>BEI du NRO</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                            <tfoot>
+
+                            <tr>
+                                <th>Code Sous-projet</th>
+                                <th>Date du Refus</th>
+                                <th>BEI du NRO</th>
+                            </tr>
+                            <tr><th colspan="3">Réseau de Distribution</th></tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- END Page Content -->
+    </div>
+
 </div>
 
 <script>
@@ -141,6 +269,10 @@ var commande_fin_travaux_ctr_dt;
 var commande_fin_travaux_cdi_dt;
 var commande_fin_travaux_hors_delai_cdi_dt;
 var commande_fin_travaux_hors_delai_ctr_dt;
+var saisie_reference_commande_fin_travaux_cdi_dt;
+var saisie_reference_commande_fin_travaux_ctr_dt;
+var commande_refuse_commande_fin_travaux_cdi_dt;
+var commande_refuse_commande_fin_travaux_ctr_dt;
     $(function () {
         // Init page plugins & helpers
     });
@@ -335,7 +467,7 @@ var commande_fin_travaux_hors_delai_ctr_dt;
                 $('#commande_fin_travaux_hors_delai_block').removeClass('block-opt-refresh');
             }
         } );
-        commande_fin_travaux_hors_delai_hors_delai_cdi_dt = $('#commande_fin_travaux_hors_delai_cdi_table1').on('preXhr.dt', function ( e, settings, data ) {
+        commande_fin_travaux_hors_delai_cdi_dt = $('#commande_fin_travaux_hors_delai_cdi_table1').on('preXhr.dt', function ( e, settings, data ) {
             $('#commande_fin_travaux_hors_delai_block').addClass('block-opt-refresh');
         }).DataTable( {
             "language": {
@@ -396,6 +528,257 @@ var commande_fin_travaux_hors_delai_ctr_dt;
                 $('#commande_fin_travaux_hors_delai_block').removeClass('block-opt-refresh');
             }
         } );
+        /**
+         * saisie reference commande fin travaux
+         */
+        saisie_reference_commande_fin_travaux_ctr_dt = $('#saisie_reference_commande_fin_travaux_ctr_table1').on('preXhr.dt', function ( e, settings, data ) {
+            $('#saisie_reference_commande_fin_travaux_block').addClass('block-opt-refresh');
+        }).DataTable( {
+            "language": {
+                "url": "assets/js/plugins/datatables/French.json"
+            },
+            "autoWidth": false,
+            "processing": true,
+            "serverSide": true,
+            "ajax": {
+                "url": 'api/dashboard/activitebei/saisie_reference_commande_fin_travaux_ctr_liste.php',
+                "method":'POST'
+            },
+            "columns": [
+                { "data": "code_sous_projet" },
+                { "data": "date_transmission_tfx" },
+                { "data": "bei_nro" }
+            ],
+            "columnDefs": [
 
+
+                {
+                    "targets": 2,
+                    "data": "lib_nro",
+                    "render": function ( data, type, full, meta ) {
+                        if(full.nom_utilisateur !=null && full.prenom_utilisateur != null )
+                            return full.nom_utilisateur + '-' + full.prenom_utilisateur;
+                        else return "n/d";
+                    }
+                },
+                {
+                    "targets": 0,
+                    orderData: [ 0, 1 ],
+                    "data": "lib_nro",
+                    "render": function ( data, type, full, meta ) {
+                        if(type == "display"){
+                            return  '<a href="?page=sousprojet&idsousprojet='+full.id_sous_projet+'">'+full.lib_nro + '-' + full.zone+'</a>';
+                        }
+
+                        return full.lib_nro + '-' + full.zone;
+                    }
+                },
+                {
+                    "targets": 1,
+                    orderData: [ 0, 1 ],
+                    "data": "date_transmission_tfx",
+                    "render": function ( data, type, full, meta ) {
+                        if(full.date_transmission_tfx !=null   ) {
+                            var dat = new Date(full.date_transmission_tfx);
+                            return dat.addDays(5);
+
+                        }
+                        else return "N/D";
+                    }
+                }
+            ],
+            "order": [[0, 'desc']]
+            ,
+            "drawCallback": function( settings ) {
+                $('#saisie_reference_commande_fin_travaux_block').removeClass('block-opt-refresh');
+            }
+        } );
+        saisie_reference_commande_fin_travaux_cdi_dt = $('#saisie_reference_commande_fin_travaux_cdi_table1').on('preXhr.dt', function ( e, settings, data ) {
+            $('#saisie_reference_commande_fin_travaux_block').addClass('block-opt-refresh');
+        }).DataTable( {
+            "language": {
+                "url": "assets/js/plugins/datatables/French.json"
+            },
+            "autoWidth": false,
+            "processing": true,
+            "serverSide": true,
+            "ajax": {
+                "url": 'api/dashboard/activitebei/saisie_reference_commande_fin_travaux_cdi_liste.php',
+                "method":'POST'
+            },
+            "columns": [
+                { "data": "code_sous_projet" },
+                { "data": "date_transmission_tfx" },
+                { "data": "bei_nro" }
+            ],
+            "columnDefs": [
+
+
+                {
+                    "targets": 2,
+                    "data": "lib_nro",
+                    "render": function ( data, type, full, meta ) {
+                        if(full.nom_utilisateur !=null && full.prenom_utilisateur != null )
+                            return full.nom_utilisateur + '-' + full.prenom_utilisateur;
+                        else return "n/d";
+                    }
+                },
+                {
+                    "targets": 0,
+                    orderData: [ 0, 1 ],
+                    "data": "lib_nro",
+                    "render": function ( data, type, full, meta ) {
+                        if(type == "display"){
+                            return  '<a href="?page=sousprojet&idsousprojet='+full.id_sous_projet+'">'+full.lib_nro + '-' + full.zone+'</a>';
+                        }
+
+                        return full.lib_nro + '-' + full.zone;
+                    }
+                },
+                {
+                    "targets": 1,
+                    orderData: [ 0, 1 ],
+                    "data": "date_transmission_tfx",
+                    "render": function ( data, type, full, meta ) {
+                        if(full.date_transmission_tfx !=null   ) {
+                            var dat = new Date(full.date_transmission_tfx);
+                            return dat.addDays(5);
+                        }
+                        else return "N/D";
+                    }
+                }
+            ],
+            "order": [[0, 'desc']]
+            ,
+            "drawCallback": function( settings ) {
+                $('#saisie_reference_commande_fin_travaux_block').removeClass('block-opt-refresh');
+            }
+        } );
+        /**
+         * Commande refusé
+         */
+        commande_refuse_commande_fin_travaux_ctr_dt = $('#commande_refuse_commande_fin_travaux_ctr_table1').on('preXhr.dt', function ( e, settings, data ) {
+            $('#commande_refuse_commande_fin_travaux_block').addClass('block-opt-refresh');
+        }).DataTable( {
+            "language": {
+                "url": "assets/js/plugins/datatables/French.json"
+            },
+            "autoWidth": false,
+            "processing": true,
+            "serverSide": true,
+            "ajax": {
+                "url": 'api/dashboard/activitebei/commande_refuse_commande_fin_travaux_ctr_liste.php',
+                "method":'POST'
+            },
+            "columns": [
+                { "data": "code_sous_projet" },
+                { "data": "date_transmission_tfx" },
+                { "data": "bei_nro" }
+            ],
+            "columnDefs": [
+
+
+                {
+                    "targets": 2,
+                    "data": "lib_nro",
+                    "render": function ( data, type, full, meta ) {
+                        if(full.nom_utilisateur !=null && full.prenom_utilisateur != null )
+                            return full.nom_utilisateur + '-' + full.prenom_utilisateur;
+                        else return "n/d";
+                    }
+                },
+                {
+                    "targets": 0,
+                    orderData: [ 0, 1 ],
+                    "data": "lib_nro",
+                    "render": function ( data, type, full, meta ) {
+                        if(type == "display"){
+                            return  '<a href="?page=sousprojet&idsousprojet='+full.id_sous_projet+'">'+full.lib_nro + '-' + full.zone+'</a>';
+                        }
+
+                        return full.lib_nro + '-' + full.zone;
+                    }
+                },
+                {
+                    "targets": 1,
+                    orderData: [ 0, 1 ],
+                    "data": "date_transmission_tfx",
+                    "render": function ( data, type, full, meta ) {
+                        if(full.date_transmission_tfx !=null   ) {
+                            var dat = new Date(full.date_transmission_tfx);
+                            return dat.addDays(5);
+
+                        }
+                        else return "N/D";
+                    }
+                }
+            ],
+            "order": [[0, 'desc']]
+            ,
+            "drawCallback": function( settings ) {
+                $('#commande_refuse_commande_fin_travaux_block').removeClass('block-opt-refresh');
+            }
+        } );
+        commande_refuse_commande_fin_travaux_cdi_dt = $('#commande_refuse_commande_fin_travaux_cdi_table1').on('preXhr.dt', function ( e, settings, data ) {
+            $('#commande_refuse_commande_fin_travaux_block').addClass('block-opt-refresh');
+        }).DataTable( {
+            "language": {
+                "url": "assets/js/plugins/datatables/French.json"
+            },
+            "autoWidth": false,
+            "processing": true,
+            "serverSide": true,
+            "ajax": {
+                "url": 'api/dashboard/activitebei/commande_refuse_commande_fin_travaux_cdi_liste.php',
+                "method":'POST'
+            },
+            "columns": [
+                { "data": "code_sous_projet" },
+                { "data": "date_transmission_tfx" },
+                { "data": "bei_nro" }
+            ],
+            "columnDefs": [
+
+
+                {
+                    "targets": 2,
+                    "data": "lib_nro",
+                    "render": function ( data, type, full, meta ) {
+                        if(full.nom_utilisateur !=null && full.prenom_utilisateur != null )
+                            return full.nom_utilisateur + '-' + full.prenom_utilisateur;
+                        else return "n/d";
+                    }
+                },
+                {
+                    "targets": 0,
+                    orderData: [ 0, 1 ],
+                    "data": "lib_nro",
+                    "render": function ( data, type, full, meta ) {
+                        if(type == "display"){
+                            return  '<a href="?page=sousprojet&idsousprojet='+full.id_sous_projet+'">'+full.lib_nro + '-' + full.zone+'</a>';
+                        }
+
+                        return full.lib_nro + '-' + full.zone;
+                    }
+                },
+                {
+                    "targets": 1,
+                    orderData: [ 0, 1 ],
+                    "data": "date_transmission_tfx",
+                    "render": function ( data, type, full, meta ) {
+                        if(full.date_transmission_tfx !=null   ) {
+                            var dat = new Date(full.date_transmission_tfx);
+                            return dat.addDays(5);
+                        }
+                        else return "N/D";
+                    }
+                }
+            ],
+            "order": [[0, 'desc']]
+            ,
+            "drawCallback": function( settings ) {
+                $('#commande_refuse_commande_fin_travaux_block').removeClass('block-opt-refresh');
+            }
+        } );
     } );
 </script>
