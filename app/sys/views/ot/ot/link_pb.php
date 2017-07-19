40af6084d5616c20796ff608bff94133a127f15f
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-07-19 15:44:34 +0000

    Rejected commit: Default

--- app/sys/views/ot/ot/link_pb.php
+++ app/sys/views/ot/ot/link_pb.php
@@ -12,6 +12,7 @@
     <br>
     <button id="link_pb" class='btn btn-info btn-sm'><span class='glyphicon glyphicon-check'>&nbsp;</span> Valider</button>
 </div>
+<div class='alert alert-success' id="message_ot_traitement_pds" role='alert' style="display: none;"></div>
 <script>
     $(function () {
         // Init page plugins & helpers
@@ -26,9 +27,10 @@
             console.log(ot_dt.row('.selected').data().id_ordre_de_travail);
 
             //$('#ot_block').addClass('block-opt-refresh');
-            $('#linked-ch-wrapper').addClass('block block-themed block-opt-refresh');
-            $('#linked-pb-wrapper').addClass('block block-themed block-opt-refresh');
-
+            $('#linked-ch-wrapper').addClass('block block-themed block-opt-refresh block-content');
+            $('#linked-pb-wrapper').addClass('block block-themed block-opt-refresh block-content');
+            var mssagetraitementpds  = {message:"PDS En cours de traitement"}
+            App.showMessage(mssagetraitementpds,'#message_ot_traitement_pds');
             $.ajax({
                 method: "POST",
                 url: "api/ot/ot/update_pb_files_list.php",
@@ -41,8 +43,8 @@
                     objtype: getObjectTypeForEntryPB(get('tentree',ot_dt))
                 }
             }).done(function (msg) {
-                $('#linked-ch-wrapper').removeClass('block-opt-refresh');
-                $('#linked-pb-wrapper').removeClass('block-opt-refresh');
+                $('#linked-ch-wrapper').removeClass('block block-themed block-opt-refresh block-content');
+                $('#linked-pb-wrapper').removeClass('block block-themed block-opt-refresh block-content');
                 App.showMessage(msg,'#message_ot_link2');
                 displayDevis();
             });
