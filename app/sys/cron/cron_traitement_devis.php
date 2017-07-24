f645f038a23eaeeebc23a6e26415aebfd74c380f
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-07-24 09:49:46 +0000

    Rejected commit: Default

---
+++ app/sys/cron/cron_traitement_devis.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * file: cron_notif_blq.php
+ * User: fadil
+ */
+//include (__DIR__."/../../../inc/config.php");
+ini_set('display_errors',1);
+require_once __DIR__.'/../../sys/libs/vendor/autoload.php';
+require_once __DIR__. "/../../sys/inc/config.php";
+require_once __DIR__. "/../../sys/inc/utils.functions.php";
+require_once __DIR__. "/../../sys/inc/mail.notifier.class.php";
+require_once __DIR__. "/../../sys/inc/ssp.class.php";
+
+
+$sql = "";
+$stm = NULL;
+$err =0;
+$sql = "select * from etat_traitement_devis where etat = 1 order by date_action DESC";
+$stm = $db->prepare($sql);
+
+
+if($stm->execute()){
+    $message [] = "cron existe";
+    $devis_traiter = $stm->fetchAll();
+
+    foreach($devis_traiter as $traitement) {
+        $Profil_execute = Utilisateur::first(
+            array('conditions' =>
+                array("id_utilisateur = ? ",$traitement['id_user'])
+            )
+        );
+        loadExcelDEF_CABLE($db,$traitement['nom_fichier'],$traitement['id_ressource'],$traitement['id_ordre_traivail'],$Profil_execute);
+
+    }
+
+
+
+
+
+} else { echo "pas d'informations à envoyer ";
+    $message [] = $stm->errorInfo();
+
+
+
+}
+
+
+echo json_encode(array("error" => $err , "message" => $message));
