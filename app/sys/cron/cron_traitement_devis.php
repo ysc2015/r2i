eac7eaaa9b581dcda24a300ebe6a0a34276200f1
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-07-24 10:08:13 +0000

    Rejected commit: Default

--- app/sys/cron/cron_traitement_devis.php
+++ app/sys/cron/cron_traitement_devis.php
@@ -4,6 +4,7 @@
  * User: fadil
  */
 //include (__DIR__."/../../../inc/config.php");
+set_time_limit(0);
 ini_set('display_errors',1);
 require_once __DIR__.'/../../sys/libs/vendor/autoload.php';
 require_once __DIR__. "/../../sys/inc/config.php";
