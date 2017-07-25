86021fa29f248e23531a629a197af554daef81ac
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-07-25 14:16:45 +0000

    Rejected commit: Default

--- app/sys/cron/cron_traitement_devis.php
+++ app/sys/cron/cron_traitement_devis.php
@@ -2,8 +2,8 @@
 /**
  * file: cron_traitement_devis.php
  * User: fadil
+ * traiter les pds valider avec leur OT il check si un cron est deja demarré avant de commencer la tache
  */
-//include (__DIR__."/../../../inc/config.php");
 set_time_limit(0);
 ini_set('display_errors',1);
 
@@ -15,45 +15,45 @@ require_once __DIR__ . "/../../sys/inc/utils.functions.php";
 require_once __DIR__."/../../sys/inc/mail.notifier.class.php";
 require_once __DIR__."/../../sys/inc/ssp.class.php";
 
-
-$sql = "";
-$stm = NULL;
+$message =[];
 $err =0;
-$sql = "select * from etat_traitement_devis where etat = 1  order by date_action DESC ";
-$stm = $db->prepare($sql);
+$sql_commence_traitement = "SELECT * FROM `cron_etat_traitement_devis` where id = 1";
+
+$stm_commence_traitement = $db->prepare($sql_commence_traitement);
+$stm_commence_traitement->execute();
+$commence_traitement = $stm_commence_traitement->fetchAll();
+if($commence_traitement['etat']=='ON'){
+    $sql = "";
+    $stm = NULL;
+    $sql = "select * from etat_traitement_devis where etat = 1  order by date_action DESC ";
+    $stm = $db->prepare($sql);
+
+    if($stm->execute()){
+        $message [] = "cron existe";
+        $devis_traiter = $stm->fetchAll();
+        $Profil_execute = null;
+        $stm_change_statut_cron = $db->prepare("update cron_etat_traitement_devis set etat = 'ON' where id = 1");
+        $stm_change_statut_cron->execute();
+        foreach($devis_traiter as $traitement) {
+
+            $Profil_execute = Utilisateur::first(
+                array('conditions' =>
+                    array("id_utilisateur = ?", $traitement['id_user'])
+                )
+            );
+            $connectedProfil = new $Profil_execute->profil->shortlib($Profil_execute);
+            loadExcelDEF_CABLE($db,$traitement['nom_fichier'],$traitement['id_ressource'],$traitement['id_ordre_traivail'],$connectedProfil);
+            loadExcelDEF_BPE_EBM($db,$traitement['nom_fichier'],$traitement['id_ressource']);
+        }
+        $stm_change_statut_cron = $db->prepare("update cron_etat_traitement_devis set etat = 'OFF' where id = 1");
+        $stm_change_statut_cron->execute();
+
+    } else { echo "pas d'informations à envoyer ";
+        $message [] = $stm->errorInfo();
 
 
-if($stm->execute()){
-    $message [] = "cron existe";
-    $devis_traiter = $stm->fetchAll();
-    $Profil_execute = null;
-    foreach($devis_traiter as $traitement) {
 
-        $Profil_execute = Utilisateur::first(
-            array('conditions' =>
-                array("id_utilisateur = ?", $traitement['id_user'])
-            )
-        );
-
-        
-
-        $connectedProfil = new $Profil_execute->profil->shortlib($Profil_execute);
-
-        //var_dump($connectedProfil);
-        
-        //echo $Profil_execute->nom_utilisateur."##".$Profil_execute->prenom_utilisateur;
-         loadExcelDEF_CABLE($db,$traitement['nom_fichier'],$traitement['id_ressource'],$traitement['id_ordre_traivail'],$connectedProfil);
-        loadExcelDEF_BPE_EBM($db,$traitement['nom_fichier'],$traitement['id_ressource']);
     }
-
-
-
-
-
-} else { echo "pas d'informations à envoyer ";
-    $message [] = $stm->errorInfo();
-
-
 
 }
 
