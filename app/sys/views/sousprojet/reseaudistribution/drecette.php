43a3937e4c413a43cedf43e5833c263c79e9ac32
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-09-06 16:11:45 +0000

    Rejected commit: Default

--- app/sys/views/sousprojet/reseaudistribution/drecette.php
+++ app/sys/views/sousprojet/reseaudistribution/drecette.php
@@ -57,11 +57,11 @@
 
                 <div class="col-md-3">
                     <label for="drec_fichier_flag">Avancement Netgeo </label><br />
-                    <input type="checkbox" class="form-control" name="drec_fichier_flag" id="drec_fichier_flag" <?=($sousProjet->distributionrecette !== NULL && $sousProjet->distributionrecette->fichier_flag != 0 ? "checked" : "")?> value="1"> Fichier Flag<br />
-                    <input type="checkbox" class="form-control" name="drec_fichier_certification" id="drec_fichier_certification" <?=($sousProjet->distributionrecette !== NULL && $sousProjet->distributionrecette->fichier_certification != 0 ? "checked" : "")?> value="1"> Fichier Certification<br />
-                    <input type="checkbox" class="form-control" name="drec_fichier_coupleur" id="drec_fichier_coupleur" <?=($sousProjet->distributionrecette !== NULL && $sousProjet->distributionrecette->fichier_coupleur != 0 ? "checked" : "")?> value="1"> Fichier Coupleur<br />
-                    <input type="checkbox" class="form-control" name="drec_base_netgeo" id="drec_base_netgeo" <?=($sousProjet->distributionrecette !== NULL && $sousProjet->distributionrecette->base_netgeo != 0 ? "checked" : "")?> value="1"> Base Netgeo<br />
-                    <input type="checkbox" class="form-control" name="drec_dedoe" id="drec_dedoe" <?=($sousProjet->distributionrecette !== NULL && $sousProjet->distributionrecette->dedoe != 0 ? "checked" : "")?> value="1"> DEDOE
+                    <input type="checkbox" class="chb" name="drec_fichier_flag" id="drec_fichier_flag" <?=($sousProjet->distributionrecette !== NULL && $sousProjet->distributionrecette->fichier_flag != 0 ? "checked" : "")?> value="1"> Fichier Flag<br />
+                    <input type="checkbox" class="chb" name="drec_fichier_certification" id="drec_fichier_certification" <?=($sousProjet->distributionrecette !== NULL && $sousProjet->distributionrecette->fichier_certification != 0 ? "checked" : "")?> value="1"> Fichier Certification<br />
+                    <input type="checkbox" class="chb" name="drec_fichier_coupleur" id="drec_fichier_coupleur" <?=($sousProjet->distributionrecette !== NULL && $sousProjet->distributionrecette->fichier_coupleur != 0 ? "checked" : "")?> value="1"> Fichier Coupleur<br />
+                    <input type="checkbox" class="chb" name="drec_base_netgeo" id="drec_base_netgeo" <?=($sousProjet->distributionrecette !== NULL && $sousProjet->distributionrecette->base_netgeo != 0 ? "checked" : "")?> value="1"> Base Netgeo<br />
+                    <input type="checkbox" class="chb" name="drec_dedoe" id="drec_dedoe" <?=($sousProjet->distributionrecette !== NULL && $sousProjet->distributionrecette->dedoe != 0 ? "checked" : "")?> value="1"> DEDOE
                 </div>
                 <div class="col-md-3">
                     <label for="drec_code_certification">Code de Certification</label>
