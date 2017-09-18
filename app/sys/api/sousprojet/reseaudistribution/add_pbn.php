636ae17e9529fbf0a355fcc86f70de17463689f2
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-09-18 09:59:28 +0000

    Rejected commit: Default

--- app/sys/api/sousprojet/reseaudistribution/add_pbn.php
+++ app/sys/api/sousprojet/reseaudistribution/add_pbn.php
@@ -13,17 +13,21 @@ $sql = "";
 $stm = NULL;
 
 $questionFieldName = "";
-
-if(!isset($pbn_information) ||  $pbn_information =="") {
+ if(!isset($pbn_information) ||  $pbn_information =="") {
     $err++;
     $message[] = "texte vide !";
+}elseif (!isset($drec_pbn_avance_netgeo) || $drec_pbn_avance_netgeo == 0 || $drec_pbn_avance_netgeo ==""){
+    $err++;
+    $message[] = "avancement Netgeo vide !";
+
 } else {
-    $sql = "INSERT INTO `pbn` ( `text_pbn`, `id_sous_projet`, `id_createur`, `date_creation`) VALUES(:text_pbn, :id_sous_projet, :id_createur, :date_creation)";
+    $sql = "INSERT INTO `pbn` ( `text_pbn`, `id_sous_projet`, `id_createur`, `date_creation`, `id_avancement_netgeo`) VALUES(:text_pbn, :id_sous_projet, :id_createur, :date_creation, :id_avancement_netgeo)";
     $stm = $db->prepare($sql);
 
     $stm->bindParam(":text_pbn",$pbn_information);
     $stm->bindParam(":id_sous_projet",$id_sous_projet);
     $stm->bindParam(':id_createur',$connectedProfil->profil->id_utilisateur);
+    $stm->bindParam(':id_avancement_netgeo',$drec_pbn_avance_netgeo);
     $stm->bindValue(':date_creation',date('Y-m-d H:i:s'));
     if($stm->execute()){
         $message [] = "Enregistrement ajouté avec succès";
--- app/sys/api/sousprojet/reseaudistribution/pbn_liste.php
+++ app/sys/api/sousprojet/reseaudistribution/pbn_liste.php
@@ -6,13 +6,14 @@
 
 extract($_GET);
 
-$table = array("pbn as t1","sous_projet as t2");
+$table = array("pbn as t1","sous_projet as t2","avancement_netgeo as t3");
 $columns = array(
     array( "db" => "t1.id_pbn", "dt" => 'id_pbn' ),
     array( "db" => "t1.text_pbn", "dt" => 'text_pbn' ),
     array( "db" => "t1.id_sous_projet", "dt" => 'id_sous_projet' ),
     array( "db" => "t1.id_createur", "dt" => 'id_createur' ),
     array( "db" => "t1.date_creation", "dt" => 'date_creation' ),
+    array( "db" => "t3.titre_avancement_netgeo", "dt" => 'titre_avancement_netgeo' ),
     array( "db" => "u.nom_utilisateur", "dt" => 'nom_utilisateur' ),
     array( "db" => "u.prenom_utilisateur", "dt" => 'prenom_utilisateur' )
 
@@ -20,6 +21,7 @@ $columns = array(
 
 $condition = "t1.id_sous_projet=t2.id_sous_projet";
 $condition .= " AND t1.id_sous_projet=".$id_sous_projet;
+$condition .= " AND t1.id_avancement_netgeo=t3.id_avancement_netgeo";
 
 
 
