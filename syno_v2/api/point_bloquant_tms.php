ae79db2aff9de71b76f3bba79bd23d081f96bcbd
iamara <iamara@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-10-25 15:21:39 +0000

    tms_final

--- syno_v2/api/point_bloquant_tms.php
+++ syno_v2/api/point_bloquant_tms.php
@@ -8,27 +8,25 @@ $result = array();
 
 switch ($action) {
     case 'type':
-    if(isset($_POST['tab_imei'])))
-    {
+
+
         $stmt = $pdo->query("SELECT * FROM point_bloquant_type_de_blocage");
         $result = $stmt->fetchAll(PDO::FETCH_OBJ);
-    }
+
     break;
 
     case 'solutions':
-    if(isset($_POST['tab_imei'])))
-    {
+
         $stmt_tmp = $pdo->query("SELECT * FROM point_bloquant_solutions_preconisees");
         $resul = $stmt_tmp->fetchAll(PDO::FETCH_OBJ);
-    }
+
     break;
 
     case 'moyens':
-    if(isset($_POST['tab_imei'])))
-    {
+
         $stmt_tmp = $pdo->query("SELECT * FROM point_bloquant_moyens_mis_en_oeuvre");
         $result = $stmt_tmp->fetchAll(PDO::FETCH_OBJ);
-    }
+    
     break;
 
 }
