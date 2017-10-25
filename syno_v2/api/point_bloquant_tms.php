563ba9812b0cb1c44dc33da69bbfd17bdfba3f14
iamara <iamara@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-10-25 15:11:49 +0000

    tms

---
+++ syno_v2/api/point_bloquant_tms.php
@@ -0,0 +1,37 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+
+include_once __DIR__ . '/ssp.class.php';
+
+
+switch ($action) {
+    case 'type':
+
+		if(isset($tab_imei))
+		{
+      $stmt = $pdo->query("SELECT * FROM point_bloquant_type_de_blocage");
+      $result = $stmt->fetch(PDO::FETCH_OBJ);
+		}
+
+    break;
+    case 'solutions':
+
+        if(isset($tab_imei))
+        {
+          $stmt_tmp = $pdo->query("SELECT * FROM point_bloquant_solutions_preconisees");
+          $resul = $stmt_tmp->fetch(PDO::FETCH_OBJ);
+        }
+        break;
+    case 'moyens':
+
+        if(isset($tab_imei))
+        {
+          $stmt_tmp = $pdo->query("SELECT * FROM point_bloquant_moyens_mis_en_oeuvre");
+          $result = $stmt_tmp->fetch(PDO::FETCH_OBJ);
+        }
+        break;
+
+}
+
+ResponseHelper::sendResponse(json_encode($result));
