7b9e1982e3bec5d12800ec1be6afaf66d92f00d8
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-11-01 16:43:30 +0000

    Rejected commit: Default

--- app/sys/api/ext/equipe/equipes_stt_generate_qrcode.php
+++ app/sys/api/ext/equipe/equipes_stt_generate_qrcode.php
@@ -13,16 +13,24 @@ require_once __DIR__."/../../../../sys/libs/vendor/qrcode/qrlib.php";
 if (!file_exists($PNG_TEMP_DIR))
     mkdir($PNG_TEMP_DIR);
 
-$filename = $PNG_TEMP_DIR.'test.png';
 
 $matrixPointSize = 6;
 $errorCorrectionLevel = 'L';
 
-QRcode::png('R2I QR Code', $filename, $errorCorrectionLevel, $matrixPointSize, 2);
+
+$equipestt_stm = $db->prepare("SELECT t1.* from equipe_stt as t1
+                        where t1.id_equipe_types IN (SELECT equipe_types.id_equipe_types FROM equipe_types WHERE a2t = 1 AND equipe_types.id_equipe_types IS NOT NULL)");
+$equipestt_stm->execute();
+
+$equipestt_list = $mailaction_stm->fetchAll();
+foreach($equipestt_list as $equipestt){
+
+    $filename = $PNG_TEMP_DIR.$equipestt_list['nom'].'.png';
+
+    QRcode::png($equipestt_list['imei'].'|'.$equipestt_list['mail'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);
 
 
-
-echo '<img src="'.$PNG_WEB_DIR.basename($filename).'" />';
-
+    echo '<img src="'.$PNG_WEB_DIR.basename($filename).'" /><br />'.$filename.'<hr />';
+}
 
 //echo json_encode(SSP::simpleJoin($_GET,$db,$table,"id_equipe_stt",$columns,$condition,""));
