e6b5106b303795bf5b86ab8741bfd1d05d441c3a
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-11-02 11:00:12 +0000

    Rejected commit: Default

---
+++ app/sys/api/ext/equipe/read_equipes_stt_qrcode.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * file: equipes_stt_generate_qrcode.php
+ * User: fadil
+ */
+
+
+extract($_POST);
+extract($_GET);
+
+$reponse = "N";
+
+if(isset($ide) && !empty($ide)&& isset($email) && !empty($email) ) {
+    $equipestt_stm = $db->prepare("SELECT t1.* from equipe_stt as t1
+                        where t1.id_equipe_stt = $ide AND t1.email = $email AND t1.id_equipe_types IN (SELECT equipe_types.id_equipe_types FROM equipe_types WHERE a2t = 1 AND equipe_types.id_equipe_types IS NOT NULL)");
+    $equipestt_stm->execute();
+
+
+     if($equipestt_stm->rowCount() > 0) {
+         echo "OUI";
+     }else{
+         echo "NO";
+     }
+
+
+}
+
+
+echo json_encode(SSP::simpleJoin($_GET,$reponse));
