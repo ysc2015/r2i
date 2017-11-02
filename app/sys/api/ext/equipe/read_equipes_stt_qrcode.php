7766d30cbcf7aa87f178eea6e25f5312198ca061
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-11-02 16:11:16 +0000

    Rejected commit: Default

--- app/sys/api/ext/equipe/read_equipes_stt_qrcode.php
+++ app/sys/api/ext/equipe/read_equipes_stt_qrcode.php
@@ -11,18 +11,38 @@ extract($_GET);
 $reponse = "N";
 
 if(isset($ide) && !empty($ide)&& isset($email) && !empty($email) ) {
-    $equipestt_stm = $db->prepare("SELECT t1.* from equipe_stt as t1
-                        where t1.id_equipe_stt = $ide AND t1.mail = '$email' AND t1.id_equipe_types IN (SELECT equipe_types.id_equipe_types FROM equipe_types WHERE a2t = 1 AND equipe_types.id_equipe_types IS NOT NULL)");
+    $equipestt_stm = $db->prepare("SELECT t1.*,t2.* from equipe_stt as t1 ,entreprises_stt t2
+                        where t2.id_entreprise = t1.id_entreprise AND t1.id_equipe_stt = $ide AND t1.mail = '$email' AND t1.id_equipe_types IN (SELECT equipe_types.id_equipe_types FROM equipe_types WHERE a2t = 1 AND equipe_types.id_equipe_types IS NOT NULL)");
     $equipestt_stm->execute();
 
      if($equipestt_stm->rowCount() > 0) {
-         echo "OUI";
+         $table_entreprise = array(
+             id_equipe_stt =>  $equipestt_stm['id_entreprise'],
+             id_equipe_stt => $equipestt_stm['nom'],
+             id_equipe_stt => $equipestt_stm['code_entreprise'],
+             id_equipe_stt => $equipestt_stm['adresse_siege']
+         );
+         $reponse = "OUI";
+
+             $content =  array
+                (  id_equipe_stt => $equipestt_stm['id_equipe_stt'],
+                  entreprise => $table_entreprise,
+                   nom =>  $equipestt_stm['nom'],
+                  prenom =>  $equipestt_stm['prenom'],
+                  mail =>  $equipestt_stm['mail'],
+                  tel =>  $equipestt_stm['tel']
+                );
+
+
+
+
+
      }else{
-         echo "NO";
+         $reponse =  "NO";
      }
 
 
 }
 
 
-echo json_encode(SSP::simpleJoin($_GET,$reponse));
+echo json_encode($_GET,$reponse,$content);
