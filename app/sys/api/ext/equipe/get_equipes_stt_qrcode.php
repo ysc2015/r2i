811551966294fd7832f24b70459feebfe9011ce1
fadelghani <fadelghani@90965971-89fd-4c9b-8651-a71ee073ffc4>
2017-11-02 10:39:02 +0000

    Rejected commit: Default

---
+++ app/sys/api/ext/equipe/get_equipes_stt_qrcode.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * file: equipes_stt_generate_qrcode.php
+ * User: fadil
+ */
+
+
+extract($_POST);
+extract($_GET);
+
+$PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'qrcode_equipe'.DIRECTORY_SEPARATOR;
+$PNG_WEB_DIR = 'qrcode_equipe/';
+require_once __DIR__."/../../../../sys/libs/vendor/qrcode/qrlib.php";
+
+if (!file_exists($PNG_TEMP_DIR))
+    mkdir($PNG_TEMP_DIR);
+
+
+$matrixPointSize = 6;
+$errorCorrectionLevel = 'L';
+if(isset($ide) && !empty($ide)) {
+    $equipestt_stm = $db->prepare("SELECT t1.* from equipe_stt as t1
+                        where t1.id_equipe_stt = $ide AND t1.id_equipe_types IN (SELECT equipe_types.id_equipe_types FROM equipe_types WHERE a2t = 1 AND equipe_types.id_equipe_types IS NOT NULL)");
+    $equipestt_stm->execute();
+
+    $equipestt_list = $equipestt_stm->fetchAll();
+    foreach($equipestt_list as $equipestt){
+
+        $filename = $PNG_TEMP_DIR.$equipestt['nom'].'.png';
+
+        QRcode::png($equipestt['id_equipe_stt'].'|'.$equipestt['mail'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);
+
+
+        echo '<img src="'.$PNG_WEB_DIR.basename($filename).'" /><br />'.$filename.'<hr />';
+    }
+}
+
+
+
+//echo json_encode(SSP::simpleJoin($_GET,$db,$table,"id_equipe_stt",$columns,$condition,""));
